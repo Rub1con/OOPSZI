f9cf590e4d1b2fcb40af81a614f10c248daf2ac7
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-02-07 14:51:16 +0300

    Add files via upload

---
+++ lab1/task1/task1/task1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+void main(double f, double x) {
+	x = 5;
+	f = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
+	cout << f << endl;
+	cin >> x;
+	f = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
+	cout << f << endl;
+	cin >> x;
+}
