bf502d99a9c7b07cdd9e2be3677e36cae6ac850a
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-02-23 23:05:48 +0300

    Add files via upload

---
+++ LAB3/task4/task4_do_while/task4_do_while.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int findFirstElement(double eps)
+{
+    int i = 0;
+    int k = 0;
+    double sum1 = 0;
+    do {
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        k = i;
+        i++;
+    } while (abs(sum1) >= eps);
+
+    return k;
+}
+
+void main(double eps)
+{
+    double res;
+    cout << "eps=";
+    cin >> eps;
+    res = findFirstElement(eps);
+    cout << res;
+
+}
