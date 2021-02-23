37fa28de04c871a7a47ab2401700cf6220fb2fd2
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-02-23 23:02:03 +0300

    Add files via upload

---
+++ LAB3/task1/task1_do_while/task1_do_while.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double sum(int n)
+{
+    double sum1 = 0;
+    int i = 0;
+    do {
+        sum1 += pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        i++;
+    } while (i < n);
+    return sum1;
+}
+
+void main(int n)
+{
+    double res;
+    cout << "n=";
+    cin >> n;
+    res = sum(n);
+    cout << res;
+
+}
---
+++ LAB3/task1/task1_for/task1_for.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double sum(int n)
+{
+    double sum1 = 0;
+
+    for (int i = 0; i < n; ++i) {
+        sum1 += pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+    }
+    return sum1;
+}
+
+void main(int n)
+{
+    double res;
+    cout << "n=";
+    cin >> n;
+    res = sum(n);
+    cout << res;
+
+}
