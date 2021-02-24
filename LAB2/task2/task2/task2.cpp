55d1bfd0853444a1969d128ba0748b3d67246375
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-02-24 12:09:23 +0300

    Add files via upload

---
+++ LAB2/task2/task2/task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>;
+
+using namespace std;
+
+double f(double x) {
+
+    x<=3? x = x * x + 3 * x + 9 : x = 1 / (pow(x, 3) + 6);
+    return x;
+}
+
+void main(void)
+{   
+    double x, res;
+
+    cout << "x=";
+    cin >> x;
+
+    res = f(x);
+
+    cout << "f(x)=" << res << endl;
+
+    system("pause");
+
+}
+
+
+
+
