3808842abf0c9479e4c320a76019faff982e7522
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-03-17 00:25:40 +0300

    Add files via upload

---
+++ LAB3/task5/task5_for/task5_for.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int findFirstNegativeElement(double eps)
+{
+    int k;
+    double sum1 = 0;
+
+    for (int i = 0;; i++) {
+        k = i;
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        if (sum1 > 0) continue;
+        if (abs(sum1) <= eps) {
+            return k;
+        }
+    }
+}
+
+void main(double eps)
+{
+    double res;
+    cout << "eps=";
+    cin >> eps;
+    res = findFirstNegativeElement(eps);
+    cout << res;
+    system("pause");
+
+}
---
+++ LAB3/task5/task5_while/task5_while.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int findFirstNegativeElement(double eps)
+{
+    int i = 0;
+    int k = 0;
+    double sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+
+    while (true) {
+        i++;
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        if (sum1 > 0) continue;
+        k = i;
+        if (abs(sum1) <= eps) {
+            return k;
+        }
+    }
+}
+
+void main(double eps)
+{
+    double res;
+    cout << "eps=";
+    cin >> eps;
+    res = findFirstNegativeElement(eps);
+    cout << res;
+    system("pause");
+
+}
