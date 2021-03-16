5e911dde3bbe5ca88110e0c0c23526a64ed2e665
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-03-17 00:22:46 +0300

    Add files via upload

---
+++ LAB3/task3/task3_do_while/task3_do_while.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print(int n, int k)
+{   
+    int i = 0;
+    double sum1 = 0;
+
+    do {
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        i++;
+        if (i % k == 0) continue;
+        cout << sum1 << endl;
+    } while (i < n);
+        
+}
+
+void main(int n, int k)
+{
+    double res;
+    cout << "n=";
+    cin >> n;
+    cin >> k;
+    print(n, k);
+
+    system("pause");
+
+}
---
+++ LAB3/task3/task3_for/task3_for.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print(int n, int k)
+{
+    double sum1 = 0;
+
+    for (int i = 0; i < n; ++i) {
+        
+        if (i % k == k-1) continue;
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+        cout << sum1 << endl;
+    }
+}
+
+void main(int n, int k)
+{
+    double res;
+    cout << "n=";
+    cin >> n;
+    cin >> k;
+    print(n, k);
+
+    system("pause");
+
+}
