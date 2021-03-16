6606397ce05acdee9c88c64f8637272ee69e25a3
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-03-17 00:21:46 +0300

    Add files via upload

---
+++ LAB3/task2/task2_do_while/task2_do_while.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double sum2(double eps)
+{
+    int i = 0;
+    double sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+    do {
+        i++;
+        sum1 = pow(-1, i) * 1 / ((i + 1) * (i + 2) * (i + 3));
+    } while (abs(sum1) >= eps);
+
+    return sum1;
+}
+
+void main(double eps)
+{
+    double res;
+    cout << "eps=";
+    cin >> eps;
+    res = sum2(eps);
+    cout << res;
+    system("pause");
+
+}
