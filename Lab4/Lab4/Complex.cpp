d2d3277e76f683b67dc7b06bcdcbe0b8f7cc34bf
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-03-22 21:29:12 +0300

    Add files via upload

---
+++ Lab4/Lab4/Complex.cpp
@@ -0,0 +1,82 @@
+#include "header.h"
+
+double Complex::getReal() const
+
+{
+
+	return this->real;
+
+}
+
+double Complex::getImaginary() const
+
+{
+
+	return this->imaginary;
+
+}
+
+void Complex::setReal(double real)
+
+{
+
+	this->real = real;
+
+}
+
+void Complex::setImaginary(double imaginary)
+
+{
+
+	this->imaginary = imaginary;
+
+}
+
+Complex::Complex(double real, double imaginary)
+
+	:real(real), imaginary(imaginary) { }
+
+ostream& operator << (ostream& stream, const Complex& z)
+
+{
+
+	if (z.getImaginary() >= 0)
+
+	{
+
+		stream << z.getReal() << "+" << z.getImaginary() << "*i";
+
+	}
+
+	else
+
+	{
+
+		stream << z.getReal() << z.getImaginary() << "*i";
+
+	}
+
+	return stream;
+
+}
+
+Complex operator +(Complex a, Complex b)
+{
+	return Complex(a.getReal() + b.getReal(), a.getImaginary() + b.getImaginary());
+}
+
+Complex operator -(Complex a, Complex b)
+{
+	return Complex(a.getReal() - b.getReal(), a.getImaginary() - b.getImaginary());
+}
+
+Complex operator *(Complex a, Complex b)
+{
+	return Complex((a.getReal() * b.getReal() - a.getImaginary() * b.getImaginary()), (a.getReal() * b.getImaginary() + a.getImaginary() * b.getReal()));
+}
+
+
+Complex sin(const Complex& z)
+{
+	return Complex(sin(z.getReal()) * cosh(z.getImaginary()), cos(z.getReal()) * sinh(z.getImaginary()));
+}
