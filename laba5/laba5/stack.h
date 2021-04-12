67c375a7a1be18bc6c0b82cb15df671cb457df67
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-04-12 23:53:38 +0300

    Add files via upload

---
+++ laba5/laba5/stack.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "iostream"
+
+using namespace std;
+
+class Stack
+{
+	int *arr;
+	int size;
+	int lastnum;
+
+public:
+	Stack(int size);
+	Stack(const Stack& other);
+	Stack(Stack&& other);
+	~Stack();
+	int GetSize() const;
+	void Push(const int& element);
+	void Pop();
+	int Peek();
+	bool CheckNoEmptyStack();
+	Stack& operator=(const Stack& other);
+	Stack& operator=(Stack&& other);
+	friend ostream& operator << (ostream& stream, const Stack& a);
+};
