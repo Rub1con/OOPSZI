8cd5729b3d97fee51b60910ce6124224bc3e071f
Rub1con <62156583+Rub1con@users.noreply.github.com>
2021-04-26 19:43:57 +0300

    Add files via upload

---
+++ Lab6/Stack2/Stack_Task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "Stack2.cpp"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    StackTemp<int>* a = new StackTemp<int>;
+    StackTemp<int>* aclass;
+    a->PushBack(1);
+    a->PushBack(2);
+    a->PushBack(3);
+    a->PushBack(4);
+    cout << "Stack: " << *a << endl;
+    cout << "Back = " << a->PeekBack() << endl;
+    cout << "Size = " << a->GetSize() << endl;
+    a->PopBack();
+    cout << "PopBackStack: " << *a << endl;
+    aclass = a;
+    cout << "New Aclass = " << *aclass << endl;
+    system("pause");
+}
